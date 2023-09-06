d8dd5eeda34d5f0dc9630baa87130d683206f20e
jhonpulido1990 <3825@holbertonstudents.com>
2023-09-06 09:32:49 -0500

    correcion 1

--- palindrome.py
+++ palindrome.py
@@ -1,52 +1,38 @@
 #!/usr/bin/python3
+def comparador(array, a, b):
+    if array[a] != array[b]:
+        if (array[a] < array[b]):
+            array[a] = array[b]
+        if (array[b] < array[a]):
+            array[b] = array[a]
+        return False
+    else:
+        return True
+
 if __name__ == "__main__":
     import sys
-    value = 0
     letter = sys.argv[1]
     arrayString = []
     n = int(sys.argv[2])
     k = int(sys.argv[3])
     differ = 0
+    palindrome = ''
+    change = 0
     for i in range(len(letter)):
         arrayString.append(int(letter[i]))
     long = n - 1
-    for j in range(int(len(arrayString) / 2)):
-        if arrayString[j] == arrayString[long]:
-            continue
-        differ += 1
+    for j in range(int(n / 2)):
+        if arrayString[j] != arrayString[long]:
+            differ += 1
         long -= 1
     if (differ > k):
         print(-1)
         exit(-1)
     long = n - 1
     init = 0
-    while (long >= init):
-        if k <= 0:
-            break;
-        if arrayString[init] == arrayString[long]:
-            if (k - 2) >= differ:
-                if arrayString[init] != 9:
-                    arrayString[init] = arrayString[long] = 9
-                    k -= 2
-            else:
-                if (k - 2) >= differ:
-                    if arrayString[init] != 9:
-                        arrayString[init] = 9
-                        k - 1
-                    if arrayString[long] != 9:
-                        arrayString[long] = 9
-                else:
-                    if arrayString[init] > arrayString[long]:
-                        arrayString[long] = arrayString[init]
-                    else:
-                        arrayString[init] = arrayString[long]
-                    k -= 1
-                differ -= 1
-            if long == init:
-                if k > 0:
-                    arrayString[long] = 9
-                    k -= 1
+    for ini in range(long):
+        comparador(arrayString, long, init)
         long -= 1
         init += 1
-        letter = ''.join(str(indexes) for indexes in arrayString)
-    print(letter)
+    k = k -differ
+    print(k)
